c38f09eb0a82399e0930c151c380d18ddba36640
Mark McQuillen <mark.mcquillen@river.co>
2016-03-13 23:12:26 -0700

    destroy burnt lanes.  Remember, capitalize OnTriggerEnter

---
+++ Assets/_Scripts/FireDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireDestroyer : MonoBehaviour {
+
+    void OnTriggerEnter (Collider ouch)
+    {
+        Debug.Log("WTF!!!");
+        Debug.Log(ouch.gameObject);
+        Destroy(ouch.gameObject);
+    }
+}
