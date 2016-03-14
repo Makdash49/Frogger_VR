b26b5d316b271e631941c9a4378f225911009514
Mark McQuillen <mark.mcquillen@river.co>
2016-03-13 22:05:29 -0700

    store highscore and display in menu scene

---
+++ Assets/_Scripts/GameState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+    public int highScore;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    void update() {
+
+    }
+
+    public void UpdateHighScore(int newScore)
+    {
+        if (newScore > highScore)
+         {
+            highScore = newScore;
+         }
+    }
+}
---
+++ Assets/_Scripts/HighScoreText.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighScoreText : MonoBehaviour {
+
+    void Start()
+    {
+        GameState state = GameObject.FindObjectOfType<GameState>();
+        Text text = GetComponent<Text>();
+        text.text = "High Score: " + state.highScore + " meters.";
+    }
+}
